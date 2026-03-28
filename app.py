50c7e5af7a34b2f27cd0c741f61e0152b2570101
Bhumika Mishra <bhumibhumikamishra@gmail.com>
2026-03-28 14:53:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+from src.data_loader import load_data
+from src.preprocessing import preprocess
+from src.content_based import recommend
+
+movies, ratings = load_data()
+data = preprocess(movies, ratings)
+
+similarity = pickle.load(open('models/similarity.pkl', 'rb'))
+
+st.title("🎬 Movie Recommendation System")
+from src.tmdb_model import load_tmdb, preprocess_tmdb, train_tmdb_model, recommend_tmdb
+tmdb = load_tmdb()
+tmdb = preprocess_tmdb(tmdb)
+similarity = train_tmdb_model(tmdb)
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Choose a movie", movie_list)
+
+if st.button("Recommend"):
+    results = recommend_tmdb(selected_movie, tmdb, similarity)
+    st.write("### Recommended Movies:")
+    for movie in results:
+        st.write(movie)
+
