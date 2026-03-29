c9c0b9eaad3380ce03df983ea95c4f76a24a28d3
Bhumika <bhumibhumikamishra@gmail.com>
2026-03-29 16:34:12 +0530

    Clean app with single TMDB pipeline

--- app/app.py
+++ app/app.py
@@ -1,31 +1,37 @@
 import sys
 import os
 
+# Fix import path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import streamlit as st
-import pickle
-from src.data_loader import load_data
-from src.preprocessing import preprocess
-from src.content_based import recommend
 
-movies, ratings = load_data()
-data = preprocess(movies, ratings)
+# Import TMDB pipeline
+from src.tmdb_model import (
+    load_tmdb,
+    preprocess_tmdb,
+    train_tmdb_model,
+    recommend_tmdb
+)
 
-similarity = pickle.load(open('models/similarity.pkl', 'rb'))
-
+# Title
 st.title("🎬 Movie Recommendation System")
-from src.tmdb_model import load_tmdb, preprocess_tmdb, train_tmdb_model, recommend_tmdb
+
+# Load and preprocess data
 tmdb = load_tmdb()
 tmdb = preprocess_tmdb(tmdb)
+
+# Train model (creates similarity matrix)
 similarity, tmdb = train_tmdb_model(tmdb)
 
-movie_list = movies['title'].values
+# Movie selection dropdown
+movie_list = tmdb['title'].values
 selected_movie = st.selectbox("Choose a movie", movie_list)
 
+# Recommendation button
 if st.button("Recommend"):
     results = recommend_tmdb(selected_movie, tmdb, similarity)
+
     st.write("### Recommended Movies:")
     for movie in results:
-        st.write(movie)
-
+        st.write(f"👉 {movie}")
